a8e7852cd3e8c6b286950b3a1e8a27b78d61a9cb
spiffytech <spiffy@spiffy.tech>
2020-11-26 16:12:09 -0500

    Put SQLite into WAL mode because why not?

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,7 @@ import * as path from "path";
 import bsql from "better-sqlite3";
 
 const db = bsql(path.join(process.env.DATA_DIR ?? process.cwd(), "db.sqlite"));
+db.exec("PRAGMA journal_mode=WAL;");
 const migrations: string[] = [
   `create table users (
         email text PRIMARY KEY not NULL,
@@ -28,5 +29,12 @@ db.transaction(() => {
   migrations.slice(schemaVersion).map((migration) => db.exec(migration));
   db.pragma(`user_version = ${migrations.length}`);
 })();
+
+// SQLite can't clean up WAL checkpoints if there's always something reading
+// from the DB. You have to manually call a checkpoint with a more advanced
+// read-blocking mechanism like RESTART, or the WAL log will grow unboundedly.
+setInterval(() => {
+  db.exec("PRAGMA wal_checkpoint(RESTART)");
+}, 60000 * 60);
 
 export default db;
