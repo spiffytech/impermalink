596d2d40ce20ff13556cfc9dd230ac4c4a20fd09
spiffytech <spiffy@spiffy.tech>
2020-11-28 21:51:59 -0500

    Stop overriding Tailwind theme. We weren't even doing it right anyway.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,7 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        gray: colors.warmGray,
-      },
-    },
-  },
+  theme: {},
   variants: {
     extend: {},
   },
